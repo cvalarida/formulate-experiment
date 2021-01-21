4d66af4fa956730711031d5d4c7c7814359ff12f
Christopher Valarida <chris.valarida@adhocteam.us>
2021-01-21 15:30:19 -0800

    Use the ReduxManager

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import StateManager from "../src/StateManager";
-import SimpleManager from "../src/simple-state";
+import ReduxManager from "../src/redux-manager/ReduxManager";
 import InputField from "../src/InputField";
 import StateViewer from "../src/state-viewer";
 
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <StateManager manager={SimpleManager}>
+        <StateManager manager={ReduxManager}>
           <InputField name={"first-name"} />
           <InputField name={"last-name"} />
           <StateViewer />
