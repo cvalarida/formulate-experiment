0c5008d4d8d4db719d5f432221340de31111d9b0
Christopher Valarida <chris.valarida@adhocteam.us>
2021-01-29 16:19:57 -0800

    Use routing

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,47 @@ import ReduxManager from "../src/redux-manager";
 import InputField from "../src/InputField";
 import StateViewer from "../src/StateViewer";
 
+import { Router, Page, Link, Chapter } from "../src/routing";
+
 import { matches } from "../src/validations";
+
+const pageOne = (
+  <>
+    <InputField label="First Name" data={"name.first"} required />
+    <InputField label="Last Name" data={"name.last"} required />
+    <InputField
+      label="How many children do you have?"
+      type="number"
+      data="childrenCount"
+      required
+    />
+    <StateViewer />
+  </>
+);
+
+const pageTwo = (
+  <>
+    <InputField label="Age" data="age" required />
+    <StateViewer />
+  </>
+);
+
+const navigation = (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/personal-information/name">Name</Link>
+    </li>
+    <li>
+      <Link to="/personal-information/age">Age</Link>
+    </li>
+    <li>
+      <Link to="/whatever">Whatever</Link>
+    </li>
+  </ul>
+);
 
 export default function Home() {
   return (
@@ -17,14 +57,23 @@ export default function Home() {
 
       <main className={styles.main}>
         <StateManager manager={ReduxManager}>
-          <InputField label="First Name" data={"name.first"} required />
-          <InputField
-            label="Last Name"
-            data={"name.last"}
-            validations={[matches("name.first")]}
-            required
-          />
-          <StateViewer />
+          <Router>
+            {navigation}
+            <Page path="/" title="Introduction">
+              In the beginning...
+            </Page>
+            <Chapter path="/personal-information" title="Personal information">
+              <Page route="/personal-information/name" title="Name">
+                {pageOne}
+              </Page>
+              <Page route="/personal-information/age" title="Age">
+                {pageTwo}
+              </Page>
+            </Chapter>
+            <Page path="/whatever">
+              <p>Well, I'm here...</p>
+            </Page>
+          </Router>
         </StateManager>
       </main>
 
